792ef2225df3dc0e4a5267825c23344b8672396d
Roland-Shum <rolandshum.shc@gmail.com>
2022-10-21 16:48:20 +0300

    commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# awsCloudwatchLogGrabbing
+A simple cli tool for grabbing latest AWS Cloudwatch log events from latest logStream and latest logGroup and filtering the result with search string.
+
+Built with Python3, Poetry, Boto3, argparse
+
+1. install poetry if you do not have it.
+2. cd to this project directory in terminal
+3. run `poetry install`
+4. run `poetry shell`
+5. goto `https://xxx.awsapps.com/start#/`
+6. login and get the AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_SESSION_TOKEN
+7. Replace the `log_group_name_prefix` and `filter_pattern` with string you want to search
+8. In the cell run the command (update the command with your parameters): `aws-log -aws_access_key_id AWS_ACCESS_KEY_ID -aws_secret_access_key AWS_SECRET_ACCESS_KEY -aws_session_token AWS_SESSION_TOKEN -region_name us-east-1 -log_group_name_prefix YOUR_PREFIX -filter_pattern "INFO"`
---
+++ script/main.py
@@ -0,0 +1,58 @@
+import argparse
+import boto3
+import json
+from datetime import datetime
+
+def start():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-aws_access_key_id', type=str, required=True)
+    parser.add_argument('-aws_secret_access_key', type=str, required=True)
+    parser.add_argument('-aws_session_token', type=str, required=True)
+    parser.add_argument('-region_name', type=str, required=True)
+    parser.add_argument('-log_group_name_prefix', type=str, required=True)
+    parser.add_argument('-filter_pattern', type=str, required=True)
+
+    args = parser.parse_args()
+
+    session = boto3.session.Session(
+        aws_access_key_id=args.aws_access_key_id,
+        aws_secret_access_key=args.aws_secret_access_key,
+        aws_session_token=args.aws_session_token,
+        region_name=args.region_name
+    )
+    client=session.client('logs')
+
+    #default returns the latest log group created
+    response=client.describe_log_groups(
+        logGroupNamePrefix=args.log_group_name_prefix,
+        limit=1
+    )
+    latest_log_group_name=response['logGroups'][0]['logGroupName']
+    latest_log_group_creation_timestamp=response['logGroups'][0]['creationTime']
+    latest_log_group_creation_datetime=datetime.fromtimestamp(latest_log_group_creation_timestamp/1000.0)
+    print('latest_log_group_name ',latest_log_group_name)
+    print('latest_log_group_creation_datetime ',latest_log_group_creation_datetime)
+
+    response = client.describe_log_streams(
+        logGroupName= latest_log_group_name,
+    #     logStreamNamePrefix='string',
+        orderBy='LastEventTime',
+        descending=True,
+        limit=1
+    )
+    latest_log_stream_name = response['logStreams'][0]['logStreamName']
+    latest_log_stream_timestamp = response['logStreams'][0]['creationTime']
+    latest_log_stream_datetime = datetime.fromtimestamp(latest_log_stream_timestamp/1000.0)
+    print('latest_log_stream_name ',latest_log_stream_name)
+    print('latest_log_stream_datetime ',latest_log_stream_datetime)
+
+    response = client.filter_log_events(
+        logGroupName=latest_log_group_name,
+        logStreamNames=[latest_log_stream_name],
+        filterPattern=args.filter_pattern,
+        limit=50,
+        interleaved=False
+    )
+
+    print("Log(s) as below:")
+    print(response)
